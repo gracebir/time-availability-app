b889dc4010eb73dd013d369f1b634dbb0f6ae141
gracebir <grace.devolop@gmail.com>
2023-08-30 12:47:08 +0200

    pages auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-
+import 'react-toastify/dist/ReactToastify.css';
+import { Poppins } from 'next/font/google'
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+
+const poppins = Poppins({ subsets: ['latin-ext'],weight: ["400", "500", "600","700"]})
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +15,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className={`${poppins.className} w-full bg-gray-100`}>
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AuthLayout from '~/Layouts/AuthLayout'
+import SignUpForm from '~/components/SignUpForm'
+
+
+const SignUp = () => {
+  return (
+    <AuthLayout>
+      <SignUpForm/>
+    </AuthLayout>
+  )
+}
+
+export default SignUp
